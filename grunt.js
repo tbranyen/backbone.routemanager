3da30626ac12cf92c482f1d0a2b4b48b847f811b
tbranyen <tim@tabdeveloper.com>
2012-03-11 01:30:18 -0500

    updated grunt to show min banner and changed date in license

--- grunt.js
+++ grunt.js
@@ -1,11 +1,19 @@
 /*global config:true, task:true*/
 config.init({
+  meta: {
+    banner: "/*!\n" + " * backbone.routemanager.js v0.1.0\n" +
+      " * Copyright 2012, Tim Branyen (@tbranyen)\n" +
+      " * backbone.routemanager.js may be freely distributed under" +
+      "the MIT license.\n */"
+  },
+
   lint: {
     files: ["grunt.js", "backbone.routemanager.js"]
   },
 
   min: {
-    "dist/backbone.routemanager.min.js": "backbone.routemanager.js"
+    "dist/backbone.routemanager.min.js": ["<banner>", 
+      "backbone.routemanager.js"]
   },
 
   watch: {
